3faff7dfd8e90d73b00adef7b7c7439a181821b3
limcpf <daeseong0226@gmail.com>
2025-02-28 00:18:49 +0900

    feat: 다국어 메시지 서비스 모듈 추가
    - 메시지 서비스 모듈 생성
    - 영어 및 한국어 로케일 JSON 파일 추가
    - 동적 메시지 로딩 및 파라미터 대체 기능 구현
    - 글로벌 모듈로 선언하여 전체 애플리케이션에서 사용 가능

---
+++ libs/messages/message.service.ts
@@ -0,0 +1,47 @@
+import { readFileSync } from "node:fs";
+import { join } from "node:path";
+import { Injectable, OnModuleInit } from "@nestjs/common";
+
+@Injectable()
+export class MessageService implements OnModuleInit {
+	private messages: Record<string, string> = {};
+	private language: string;
+
+	async onModuleInit() {
+		this.language = process.env.LANGUAGE || "en"; // 기본값 영어
+		this.loadMessages();
+	}
+
+	private loadMessages() {
+		try {
+			const filePath = join(
+				__dirname,
+				"locales",
+				`${this.language ?? "en"}.json`,
+			);
+			const fileContent = readFileSync(filePath, "utf-8");
+			this.messages = JSON.parse(fileContent);
+		} catch (error) {
+			switch (this.language) {
+				case "ko":
+					throw new Error("메세지 공통 모듈 로드 실패");
+				default:
+					throw new Error(
+						`Failed to load messages, this.language: ${this.language ?? "NULL"}`,
+					);
+			}
+		}
+	}
+
+	getPlainMessage(key: string): string {
+		return this.messages[key] ?? key;
+	}
+
+	getMessage(key: string, ...params: string[]): string {
+		let message = this.messages[key] || key; // 메시지가 없으면 키를 그대로 반환
+		params.forEach((param, index) => {
+			message = message.replace(`\${${index + 1}}`, param);
+		});
+		return message;
+	}
+}
