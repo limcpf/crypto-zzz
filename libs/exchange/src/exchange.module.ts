abd25bba00cacf50e7c389f83e8cd7b080e1d57e
limcpf <daeseong0226@gmail.com>
2025-03-06 12:31:42 +0900

    feat: 거래소 모듈 동적 등록 구현

---
+++ libs/exchange/src/exchange.module.ts
@@ -0,0 +1,29 @@
+import { ExchangeFactory } from "@libs/exchange/src/factory/exchange.factory";
+import { ExchangeType } from "@libs/exchange/src/models/common.model";
+import { DynamicModule, Module } from "@nestjs/common";
+import { ExchangeService } from "./exchange.service";
+
+@Module({})
+export class ExchangeModule {
+	/**
+	 * 기본 모듈 등록
+	 * @returns 모듈 설정
+	 */
+	static register(
+		exchangeType: ExchangeType = ExchangeType.UPBIT,
+	): DynamicModule {
+		return {
+			module: ExchangeModule,
+			providers: [
+				ExchangeFactory,
+				ExchangeService,
+				// 토큰 등록
+				{
+					provide: "EXCHANGE_TYPE",
+					useValue: exchangeType,
+				},
+			],
+			exports: [ExchangeService],
+		};
+	}
+}
