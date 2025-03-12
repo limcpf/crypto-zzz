f54ff6f66510f29a805321a82a414003b3952281
limcpf <daeseong0226@gmail.com>
2025-03-12 22:17:30 +0900

    feat: 웹훅 로깅을 위한 환경 설정 및 인터페이스 추가

--- apps/candle-save/src/main.ts
+++ apps/candle-save/src/main.ts
@@ -1,13 +1,8 @@
-import { ConsoleLogger } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 import { CandleSaveModule } from "./candle-save.module";
 
 async function bootstrap() {
-	const app = await NestFactory.create(CandleSaveModule, {
-		logger: new ConsoleLogger({ json: true }),
-	});
+	const app = await NestFactory.create(CandleSaveModule);
 	await app.listen(process.env.port ?? 3000);
-
-	console.log("DISCORD_INFO_WEBHOOK_URL", process.env.DISCORD_INFO_WEBHOOK_URL);
 }
 bootstrap();
