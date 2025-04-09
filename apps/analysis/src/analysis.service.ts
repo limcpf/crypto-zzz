41073246d1b83f4ff2166e2f8883d11e2ef75683
limcpf <daeseong0226@gmail.com>
2025-04-09 22:22:37 +0900

    feat: scoring 초안 작성

--- apps/analysis/src/analysis.service.ts
+++ apps/analysis/src/analysis.service.ts
@@ -1,12 +1,20 @@
 import {Injectable} from '@nestjs/common';
 import {CoinLogger} from "@libs/logger/coin-logger";
+import {ScoringFactory} from "@apps/analysis/src/strategies/strategy.factory";
 
 @Injectable()
 export class AnalysisService {
   constructor(
       private readonly logger: CoinLogger, // 코인 로거 Coin Logger
+      private readonly scoringFactory: ScoringFactory,
+      private readonly strategies: string[]
   ) {
     this.logger.setContext("AnalysisService");
+
+    // TODO: main.ts에 설정에 있는 전략은 몇개고 실제 불러와진건 몇갠지 테스트?
+
+    const enabledStrategies = process.env.ENABLED_STRATEGIES || "ma";
+    this.strategies = enabledStrategies.split(",").filter(s => s);
   }
 
   /**
@@ -14,9 +22,27 @@ export class AnalysisService {
    * @param msg - 코인 정보
    * @returns 캔들 데이터에 따른 스코어링 점수
    */
-  async scoring(msg: any): Promise<number> {
-    console.log(msg);
-    return 0;
+  async scoring(msg: unknown): Promise<number> {
+    const { coin }: { coin: string } = JSON.parse(msg as string);
+
+    let score = 0;
+
+    for(const s of this.strategies) {
+      const strategy = this.scoringFactory.getStrategy(s);
+
+      let result = await strategy.execute(coin);
+
+      if(Number.isNaN(result)) {
+        this.logger.debug(`[${coin} - ${s}] is NaN`);
+      } else {
+        this.logger.debug(`[${coin} - ${s}] score : ${result}`);
+        score += result;
+      }
+    }
+
+    this.logger.debug(`final score : ${score}`);
+
+    return score
   }
 
   /**
